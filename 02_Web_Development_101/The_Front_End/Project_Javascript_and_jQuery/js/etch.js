82255866939c25caa1004bf2c120e23a27432d62
Sergio <sbadillo@gmail.com>
2017-06-03 22:57:20 +0200

    project etch-a-sketch finished

---
+++ 02_Web_Development_101/The_Front_End/Project_Javascript_and_jQuery/js/etch.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){
+
+	reset();
+   	fillPalette();
+
+	// default color:
+	var currentColor = '#000';
+	$("#color").css('background-color', currentColor); 
+
+	// color change
+	$(".colorblock").mouseover(function(event) {
+		currentColor = $(this).css('background-color');
+		$("#color").css('background-color', currentColor); 
+	});
+
+	// draw
+	$(".block").mouseover(function(event) {
+		$(this).css("background-color", currentColor);
+		console.log('drawing!')
+	});
+
+	$('#reset').click(function(event) {
+		/* Act on the event */
+		reset();
+			// draw
+		$(".block").mouseover(function(event) {
+			$(this).css("background-color", currentColor);
+			console.log('drawing!')
+	});
+	});
+
+});
+
+
+// Create grid
+function createGrid(etchsize,rowsize){
+
+	$('#info').text("grid size: "+rowsize+"x"+rowsize);
+
+	$('#etch-container').empty();
+
+
+	$('#etch-container').css("width", etchsize);
+
+	var size = rowsize;
+	var $etch = $('#etch-container')
+	var block = ("<div class='block'></div>")
+
+	for (var i = 0; i < size*size; i++) {
+		$etch.append(block);
+	}
+
+	// adapt size of each block
+	var blocksize = $etch.width() / size;
+	$(".block").css("width", blocksize);
+	$(".block").css("height", blocksize);
+}
+
+function fillPalette(){
+	$('#palette').append("<div class='colorblock' style='background-color:white'></div>");
+	$('#palette').append("<div class='colorblock' style='background-color:black'></div>");
+
+	for (var i = 0; i < 8; i++) {
+		c = getRandomColor();
+		var colorblock = ("<div class='colorblock' style='background-color:"+c+"'></div>")
+		$('#palette').append(colorblock);
+	}
+}
+
+function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++ ) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+function reset(){
+    var txt;
+    var size = prompt("How many squares per sides you want?:", "16");
+    if (size == null || size == "") {
+        createGrid(960,16);
+    } else {
+        createGrid(960,size);
+    }
+    
+}
+
+
